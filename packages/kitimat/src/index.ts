97657a291f66cd8c2735d6a5681bca9b029adb28
Gabe Scholz <scholz.gabe@gmail.com>
2018-03-22 18:07:34 -0700

    Expose Shrink too

--- packages/kitimat/src/index.ts
+++ packages/kitimat/src/index.ts
@@ -2,8 +2,9 @@ import * as Options from './options';
 import * as RoseTree from './rosetree';
 import * as Iter from './iterable';
 import * as Random from './random';
+import * as Shrink from './shrink';
 
 export { property } from './property';
 export { check } from './check';
-export { Options, RoseTree, Iter, Random };
+export { Options, RoseTree, Iter, Random, Shrink };
 export * from './fuzz';
